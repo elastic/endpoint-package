a2990e5313165c451aec871fbb28406c4c821aa3
Marshall Main <55718608+marshallmain@users.noreply.github.com>
2020-07-01 10:06:18 -0400

    Add exceptionable malware fields (#23)
    * Add execptionable annotations to malware subset
    
    * Generate list of exceptionable fields

---
+++ scripts/exceptions/main.py
@@ -0,0 +1,34 @@
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
+
+import argparse
+import yaml
+import json
+import glob
+
+
+def argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('schema', help='path to the schema to parse')
+
+    return parser.parse_args()
+
+def main():
+    args = argument_parser()
+    exceptionable_fields = []
+    subset_dirs = glob.glob(args.schema + '/generated/ecs/subset/*')
+    for subset in subset_dirs:
+        with open(subset + '/ecs_flat.yml') as f:
+            flat = yaml.safe_load(f)
+        for (field, options) in flat.items():
+            if 'exceptionable' in options:
+                exceptionable_fields.append(field)
+        if exceptionable_fields:
+            with open(subset + '/exceptionable.json', 'w') as output:
+                json.dump(exceptionable_fields, output, indent=2)
+
+if __name__ == '__main__':
+    main()
