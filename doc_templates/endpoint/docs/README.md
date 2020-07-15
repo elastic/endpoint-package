6a20b191c512858a83deb70dcccdedece2e7b129
Jonathan Buttner <jonathan.buttner@elastic.co>
2020-07-15 14:41:48 -0400

    Generating readme

---
+++ doc_templates/endpoint/docs/README.md
@@ -0,0 +1,59 @@
+# Elastic Endpoint Security Integration
+
+This integration sets up templates and index patterns required for Elastic Endpoint Security.
+
+## Compatibility
+
+For compatibility information view our [documentation](https://www.elastic.co/guide/en/security/current/index.html).
+
+## Logs
+
+The log type of documents are stored in the `logs-endpoint.*` indices. The following sections define the mapped fields
+sent by the endpoint.
+
+### alerts
+
+{{fields "alerts"}}
+
+### file
+
+{{fields "file"}}
+
+### library
+
+{{fields "library"}}
+
+### network
+
+{{fields "network"}}
+
+### process
+
+{{fields "process"}}
+
+### registry
+
+{{fields "registry"}}
+
+### security
+
+{{fields "security"}}
+
+## Metrics
+
+The metrics type of documents are stored in `metrics-endpoint.*` indices. The following sections define the mapped fields
+sent by the endpoint.
+
+### metadata
+
+{{fields "metadata"}}
+
+### metrics
+
+Metrics documents contain performance information about the endpoint executable and the host it is running on.
+
+{{fields "metrics"}}
+
+### policy response
+
+{{fields "policy"}}
---
+++ scripts/generate-docs/main.go
@@ -0,0 +1,76 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+type generateOptions struct {
+	docTemplatesDir   string
+	packages          string
+	packagesSourceDir string
+}
+
+func (o *generateOptions) validate() error {
+	_, err := os.Stat(o.packagesSourceDir)
+	if err != nil {
+		return errors.Wrapf(err, "stat file failed for packages directory (path: %s)", o.packagesSourceDir)
+	}
+
+	_, err = os.Stat(o.docTemplatesDir)
+	if err != nil {
+		return errors.Wrapf(err, "stat file failed for doc templates (path: %s)", o.docTemplatesDir)
+	}
+
+	return nil
+}
+
+func (o *generateOptions) selectedPackages() []string {
+	var selected []string
+	p := strings.TrimSpace(o.packages)
+	if len(p) > 0 {
+		selected = strings.Split(p, ",")
+	}
+	return selected
+}
+
+func main() {
+	var options generateOptions
+	flag.StringVar(&options.docTemplatesDir, "templates", "./doc_templates", "Path to the README templates directory")
+	flag.StringVar(&options.packages, "packages", "endpoint", "Packages selected for generating docs")
+	flag.StringVar(&options.packagesSourceDir, "sourceDir", "./package", "Path to the packages directory")
+	flag.Parse()
+
+	err := options.validate()
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "command options validation failed"))
+	}
+
+	err = generateDocs(options)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "generating docs failed"))
+	}
+}
+
+func generateDocs(options generateOptions) error {
+	packages, err := listPackages(options)
+	if err != nil {
+		return errors.Wrap(err, "listing packages failed")
+	}
+
+	for _, packageName := range packages {
+		err = renderReadme(options, packageName)
+		if err != nil {
+			return errors.Wrapf(err, "rendering README file failed (packageName: %s)", packageName)
+		}
+	}
+	return nil
+}
