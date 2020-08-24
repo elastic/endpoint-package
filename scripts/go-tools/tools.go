4c7be8f874214673c70ec307e3e99884f8240dd0
Jonathan Buttner <jonathan.buttner@elastic.co>
2020-08-24 16:05:16 -0400

    Adding more docs

--- scripts/go-tools/tools.go
+++ scripts/go-tools/tools.go
@@ -4,6 +4,13 @@
 
 // +build tools
 
+/**
+ * This file is a dummy file that will never be compiled. It just indicates that we're using
+ * the elastic-package tool. This is necessary so we can vendor it. If additional external go tools
+ * are needed they can be added in the import here.
+ *
+ * For more information see: https://github.com/go-modules-by-example/index/blob/master/010_tools/README.md
+ */
 package tools
 
 import (
