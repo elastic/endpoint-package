b407e21b279ef0b96ffe33c4524239e3e7c09f19
Jonathan Buttner <jonathan.buttner@elastic.co>
2020-08-26 17:06:11 -0400

    Test delete branch

--- scripts/release_manager/main.py
+++ scripts/release_manager/main.py
@@ -142,7 +142,10 @@ def push_commits(repo, remote, local_branch, upstream_branch):
 @click.option('--env', required=True, prompt='Is this a dev or prod release?', default='dev', type=click.Choice(
     ['dev', 'prod'], case_sensitive=False))
 def main(package_storage_path, package_dir, env):
-    click.echo('env: {}'.format(env))
+    delete_old_branch(local_repo, 'blahblah')
+    
+    import sys
+    sys.exit()
     add_remote(local_repo, UPSTREAM, 'git@github.com:elastic/endpoint-package.git')
     upstream_branch = get_upstream_branch(local_repo)
 
