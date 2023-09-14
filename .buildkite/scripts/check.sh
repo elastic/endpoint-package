72e81e71dd616dfd4d79421ad6592e19b8765dd9
Chan Kim <5281995+gogochan@users.noreply.github.com>
2023-09-14 13:40:35 -0400

    python3

--- .buildkite/scripts/check.sh
+++ .buildkite/scripts/check.sh
@@ -8,7 +8,7 @@ sudo apt update -y && sudo apt install -y python3-requests golang-go
 
 echo "--- Retrieving stack version"
 # Use STACK_VERSION if defined, else use the output of .buildkite/scripts/find_oldest_supported_version.py
-_STACK_VERSION=${STACK_VERSION:-$(python .buildkite/scripts/find_oldest_supported_version.py)}
+_STACK_VERSION=${STACK_VERSION:-$(python3 .buildkite/scripts/find_oldest_supported_version.py)}
 echo "Using stack version $_STACK_VERSION"
 
 echo "--- Prepare stack"
