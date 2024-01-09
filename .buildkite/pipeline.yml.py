1d6bdf32ca2bfbbd686547999725589e0834a29a
Chan Kim <5281995+gogochan@users.noreply.github.com>
2024-01-09 07:32:00 -0500

    mock test run

--- .buildkite/pipeline.yml.py
+++ .buildkite/pipeline.yml.py
@@ -57,7 +57,8 @@ def main():
         },
     ]
 
-    if current_branch == "main" or re.match(r"^[78]\.\d+$", current_branch):
+    #if current_branch == "main" or re.match(r"^[78]\.\d+$", current_branch):
+    if current_branch == "bk/publishing":
         steps.append({
                 "label": "Check if published",
                 "command": ".buildkite/scripts/sign_and_publish.sh --check",
