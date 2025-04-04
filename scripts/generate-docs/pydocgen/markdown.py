7c6ffac563f5dc15c7accb68018d8366b6ebde7b
Brian McKinney <brian.mckinney@elastic.co>
2025-04-04 15:46:42 +0000

    variant 4

--- scripts/generate-docs/pydocgen/markdown.py
+++ scripts/generate-docs/pydocgen/markdown.py
@@ -275,23 +275,38 @@ def generate_custom_documentation_markdown(
                             has_example=bool(meta["ecs"]["example"]),
                         )
 
-                    f.write(f"#### `{field}`\n\n")
-                    f.write("<table>\n")
+
+
+                    f.write(f"### `{field}`\n\n")
                     if meta["ecs"]["description"]:
-                        f.write(f"<tr><td><strong>Description</strong></td><td>{meta['ecs']['description']}</td></tr>\n")
+                        f.write("#### Description\n\n")
+                        f.write(f">{meta['ecs']['description']}\n\n")
                     if meta["endpoint"]["description"]:
-                        f.write(
-                            f"<tr><td><strong>Endpoint Description</strong></td><td>{meta['endpoint']['description']}</td></tr>\n"
-                        )
+                        f.write("#### Endpoint Description\n\n")
+                        f.write(f"> {meta['endpoint']['description']}\n\n")
                     if meta["endpoint"]["example"]:
-                        f.write(
-                            f"<tr><td><strong>ECS Example</strong></td><td><code>{meta['endpoint']['example']}</code></td></tr>\n"
-                        )
+                        f.write("#### Example\n\n")
+                        f.write(f">{meta['endpoint']['example']}\n\n")
                     elif meta["ecs"]["example"]:
-                        f.write(
-                            f"<tr><td><strong>Endpoint Example</strong></td><td><code>{meta['ecs']['example']}</code></td></tr>\n"
-                        )
-                    f.write("</table>\n\n")
+                        f.write("#### Example\n\n")
+                        f.write(f">{meta['ecs']['example']}\n\n")
+
+                    # f.write("<table>\n")
+                    # if meta["ecs"]["description"]:
+                    #     f.write(f"<tr><td><strong>Description</strong></td><td>{meta['ecs']['description']}</td></tr>\n")
+                    # if meta["endpoint"]["description"]:
+                    #     f.write(
+                    #         f"<tr><td><strong>Endpoint Description</strong></td><td>{meta['endpoint']['description']}</td></tr>\n"
+                    #     )
+                    # if meta["endpoint"]["example"]:
+                    #     f.write(
+                    #         f"<tr><td><strong>ECS Example</strong></td><td><code>{meta['endpoint']['example']}</code></td></tr>\n"
+                    #     )
+                    # elif meta["ecs"]["example"]:
+                    #     f.write(
+                    #         f"<tr><td><strong>Endpoint Example</strong></td><td><code>{meta['ecs']['example']}</code></td></tr>\n"
+                    #     )
+                    # f.write("</table>\n\n")
 
             logging.debug(f"wrote markdown to {output_filename}")
 
